fd96c6cbc07fde3faaf3cd5e4a0591fa5d7242fb
fog <20683340+dreamboy1339@users.noreply.github.com>
2026-02-07 20:38:55 +0900

    `feat: Spring Boot 기반 Todo API 초기 프로젝트 구성(Gradle 설정, InMemory CRUD, Swagger, 테스트 추가)`

---
+++ src/test/java/com/asdf/todo/service/TodoServiceTests.java
@@ -0,0 +1,69 @@
+package com.asdf.todo.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.asdf.todo.model.Todo;
+import com.asdf.todo.repository.TodoInMemoryRepository;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TodoServiceTests {
+
+    @Autowired private TodoService todoService;
+
+    @BeforeEach
+    void setUp() {
+        todoService = new TodoService(new TodoInMemoryRepository());
+        todoService.save(new Todo(null, "Test Todo 1", "Description 1", false));
+        todoService.save(new Todo(null, "Test Todo 2", "Description 2", false));
+    }
+
+    @AfterEach
+    void tearDown() {}
+
+    @Test
+    void testFindAll() throws Exception {
+        List<Todo> todos = todoService.findAll();
+
+        assertThat(todos).hasSize(2);
+    }
+
+    @Test
+    void testSaveTodo() throws Exception {
+        Todo todo = new Todo(null, "New Todo", "New Description", false);
+        todoService.save(todo);
+
+        assertThat(todoService.findAll()).hasSize(3);
+    }
+
+    @Test
+    void testFindById() throws Exception {
+        Todo todo = todoService.findById(1L);
+
+        assertThat(todo).isNotNull();
+        assertThat(todo.getTitle()).isEqualTo("Test Todo 1");
+    }
+
+    @Test
+    void testUpdateTodo() throws Exception {
+        Todo updatedTodo = new Todo(1L, "Updated Todo", "Updated Description", true);
+        todoService.update(1L, updatedTodo);
+        Todo todo = todoService.findById(1L);
+
+        assertThat(todo.getTitle()).isEqualTo("Updated Todo");
+        assertThat(todo.getDescription()).isEqualTo("Updated Description");
+        assertThat(todo.isCompleted()).isTrue();
+    }
+
+    @Test
+    void testDeleteTodo() throws Exception {
+        todoService.delete(1L);
+        assertThat(todoService.findAll()).hasSize(1);
+        assertThat(todoService.findById(1L)).isNull();
+    }
+}
